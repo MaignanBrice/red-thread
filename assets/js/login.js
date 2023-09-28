394c03379f658648dd61cabff347e391e463f46b
Brice Maignan <mgnn.brice@pm.me>
2023-09-28 21:00:43 +0200

    Login done

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+document.querySelector('#id-btn').addEventListener('click', () => {
+    checkPassword(document.querySelector('.form--pwd').value);
+});
+
+localStorage.setItem('id', 'briceM');
+localStorage.setItem('pwd', 'password');
+
+
+function checkPassword(value) {
+    if (value === localStorage.getItem('pwd')) window.location.href = "/index.html"
+}
+
