fdcb156f8755cf3350a9f44c9f904b09e72a9b36
Brice Maignan <mgnn.brice@pm.me>
2023-10-24 16:03:52 +0200

    fixed scroll overflox on main index.html

--- assets/js/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#burger-btn').addEventListener('click', () => {
-    document.querySelector('.nav--mobile').classList.remove('hidden');
-    document.querySelector('.fader--mobile').classList.remove('hidden');
-});
-
-document.querySelector('.fader--mobile').addEventListener('click', (event) => {
-    event.target.classList.add('hidden');
-    document.querySelector('.nav--mobile').classList.add('hidden');
-})
